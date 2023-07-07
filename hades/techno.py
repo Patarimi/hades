a81da859b37245fe3ed948990b7b7ae7a7d13ffd
Patarimi <mpqqch@gmail.com>
2023-07-07 18:41:34 +0200

    [WIP] add pdk install for sky130a support and remove asap7

--- hades/techno.py
+++ hades/techno.py
@@ -1,6 +1,32 @@
 import re
 import yaml
-from os.path import join, dirname
+from os.path import join, dirname, isdir
+from os import makedirs
+import urllib.request
+import tarfile
+from typer import Typer
+
+pkd_app = Typer()
+
+
+@pkd_app.command("install")
+def install():
+    """
+    install the sky130a technology in its default location.
+    """
+    base_install = join(dirname(__file__), "../pdk/")
+    pdk_name = "sky130a"
+    base_url = "https://anaconda.org/LiteX-Hub/open_pdks.sky130a/1.0.421_0_gb662727/download/noarch/open_pdks.sky130a-1.0.421_0_gb662727-20230606_125334.tar.bz2"
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)')]
+    urllib.request.install_opener(opener)
+    print("downloading files, might take some times...")
+    urllib.request.urlretrieve(base_url, base_install+pdk_name+".tar.bz2")
+    if not (isdir(base_install + pdk_name)):
+        makedirs(base_install + pdk_name)
+    print("extracting, please wait...")
+    with tarfile.open(base_install + pdk_name + ".tar.bz2", mode="r") as bz:
+        bz.extractall(base_install + pdk_name)
 
 
 def load(techno: str):

62b62deb74288002b266a8a2c4774485e094e500
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-07-13 14:28:37 +0200

    [Fix] create dir before using it Remove archive after usage fix techno.yml

--- hades/techno.py
+++ hades/techno.py
@@ -1,3 +1,4 @@
+import os
 import re
 import yaml
 from os.path import join, dirname, isdir
@@ -17,16 +18,17 @@ def install():
     base_install = join(dirname(__file__), "../pdk/")
     pdk_name = "sky130a"
     base_url = "https://anaconda.org/LiteX-Hub/open_pdks.sky130a/1.0.421_0_gb662727/download/noarch/open_pdks.sky130a-1.0.421_0_gb662727-20230606_125334.tar.bz2"
+    if not (isdir(base_install + pdk_name)):
+        makedirs(base_install + pdk_name)
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)')]
     urllib.request.install_opener(opener)
     print("downloading files, might take some times...")
     urllib.request.urlretrieve(base_url, base_install+pdk_name+".tar.bz2")
-    if not (isdir(base_install + pdk_name)):
-        makedirs(base_install + pdk_name)
     print("extracting, please wait...")
     with tarfile.open(base_install + pdk_name + ".tar.bz2", mode="r") as bz:
         bz.extractall(base_install + pdk_name)
+    os.remove(base_install+pdk_name+".tar.bz2")
 
 
 def load(techno: str):

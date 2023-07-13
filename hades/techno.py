4d31c97654543737ba4bca96b3b0f481d9d4b32c
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-07-13 18:02:43 +0200

    add more pdks

--- hades/techno.py
+++ hades/techno.py
@@ -4,31 +4,47 @@ import yaml
 from os.path import join, dirname, isdir
 from os import makedirs
 import urllib.request
-import tarfile
+import tarfile, zipfile
 from typer import Typer
 
 pkd_app = Typer()
 
 
 @pkd_app.command("install")
-def install():
+def install(pdk_name: str):
     """
     install the sky130a technology in its default location.
     """
     base_install = join(dirname(__file__), "../pdk/")
-    pdk_name = "sky130a"
-    base_url = "https://anaconda.org/LiteX-Hub/open_pdks.sky130a/1.0.421_0_gb662727/download/noarch/open_pdks.sky130a-1.0.421_0_gb662727-20230606_125334.tar.bz2"
+    tech = load(pdk_name)
+    base_url = tech["source_url"]
     if not (isdir(base_install + pdk_name)):
         makedirs(base_install + pdk_name)
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)')]
     urllib.request.install_opener(opener)
     print("downloading files, might take some times...")
-    urllib.request.urlretrieve(base_url, base_install+pdk_name+".tar.bz2")
+    ext = ".zip" if ".zip" in base_url else ".tar.bz2"
+    file_name=base_install+pdk_name+ext
+    urllib.request.urlretrieve(base_url, file_name)
     print("extracting, please wait...")
-    with tarfile.open(base_install + pdk_name + ".tar.bz2", mode="r") as bz:
-        bz.extractall(base_install + pdk_name)
-    os.remove(base_install+pdk_name+".tar.bz2")
+    if ext == ".tar.bz2":
+        with tarfile.open(file_name, mode="r") as bz:
+            bz.extractall(base_install + pdk_name)
+    else:
+        with zipfile.open(file_name, mode="r") as zp:
+            zp.extractall(base_install+pdk_name)
+    os.remove(file_name)
+
+
+@pkd_app.command("list")
+def list_pdk():
+    tech_yml = join(dirname(__file__), "techno.yml")
+    with open(tech_yml, "r") as f:
+        process_d = yaml.load(f, Loader=yaml.Loader)
+    print("Available PDKs are:")
+    for k in process_d:
+        print(f"- {k}")
 
 
 def load(techno: str):

29e845ee0cd32a5a4170a76744d12b26c456b240
Patarimi <mpqqch@gmail.com>
2023-07-19 10:54:38 +0200

    add equation directory to store empirical equation

---
+++ hades/equation/inductor.py
@@ -0,0 +1,16 @@
+from numpy import pi
+
+
+WHEELER_REF = {"hexagonal": (2.3, 3.83)}
+
+
+def wheeler(n: int, d_avg: float, rho: float, k: tuple, shape: str = "custom"):
+    u_0 = 4 * pi * 1e-7
+    if shape == "custom":
+        k1, k2 = k
+    else:
+        try:
+            k1, k2 = WHEELER_REF[shape]
+        except KeyError:
+            raise KeyError(f"Unknown shape, available shape are: {WHEELER_REF.keys()}")
+    return k1 * u_0 * d_avg * n**2 / (1 + k2 * rho)

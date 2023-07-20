de0ce03b79543ed48d9ca0eef74ef65d56f7a1fc
Patarimi <mpqqch@gmail.com>
2023-07-20 14:45:52 +0200

    add micro-strip line equation and tests

--- tests/test_equation.py
+++ tests/test_equation.py
@@ -1,4 +1,5 @@
 import hades.equation.inductor as ind
+import hades.equation.micro_strip as ms
 
 
 def test_inductor():
@@ -9,3 +10,18 @@ def test_inductor():
     for i, n in enumerate((1, 2)):
         for j, d in enumerate((50e-6, 80e-6)):
             assert ind.wheeler(n, d, 0.1, "hexagonal") == ref[i][j]
+
+
+def test_micro_strip():
+    ref = (
+        80.84395749319764,
+        50.052965553475644,
+        59.58804940235251,
+        113.70422352016742,
+    )
+    for i, w in enumerate((5, 12, 9, 2)):
+        assert ms.wheeler(w * 1e-6, 4e-6, 2, 1e-6) == (ref[i], 0)
+    ref_d = (4.540148543324036e-14, 2.2700742716620183e-13, 4.086133688991633e-12)
+    for i, l in enumerate((10, 50, 900)):
+        _, delay = ms.wheeler(12e-6, 4e-6, 2, 1e-6, l * 1e-6)
+        assert delay == ref_d[i]

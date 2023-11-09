6c8c65e43c1bda63784c0aafa7092fb2f3eb20f5
Patarimi <38954040+Patarimi@users.noreply.github.com>
2023-11-09 15:54:45 +0100

    [WIP] add stepped impedance and scaling

--- hades/devices/app_models/filter.py
+++ hades/devices/app_models/filter.py
@@ -49,55 +49,82 @@ def prototype(order: int, style: str, ripple: float = 0.2):
     raise ValueError(f"Unkwon filter type {style}, available are flat, ripple")
 
 
-def scaling(prototype: list[float], f: float, R_0: float):
+def scaling(prototype: np.array, f: float, r_0: float):
     """
     properly scale a low-pass filter prototype.
-    :param prototype:
+    :param prototype: list of coefficient of the prototype (see [prototype](#hades.devices.app_models.filter.prototype))
     :param f:
-    :param R_0:
+    :param r_0:
     :return:
     """
-    return 1
+    w_c = 2 * np.pi * f
+    denorm_f = prototype / w_c
+    denorm = list()
+    for i, d in enumerate(denorm_f):
+        denorm.append(d * r_0 if i % 2 == 1 else d / r_0)
+    return np.array(denorm)
 
 
-norm_coeff = prototype(order, style)
+def to_stepped_impedance(
+    prototype: np.array, z_high: float, z_low: float, r_0: float = 50
+):
+    """
+    Convert a low-pass prototype to a stepped_impedance line filter. The exact function is implemented instead of the
+     approximation presented in Pozar 2012 chapter. 8.6.
+    :param prototype: prototype of the filter
+    :param z_high: impedance of the high impedance section
+    :param z_low: impedance of the low impedance section
+    :param r_0: characteristic impedance
+    :return: list of required delay in degrees ($beta l$)
+    """
+    denorm = list()
+    for i, g in enumerate(prototype):
+        if i % 2 == 1:
+            # approx -> denorm.append(g * r_0 / z_high)
+            denorm.append(2 * np.arctan(g * r_0 / (2 * z_high)))
+        else:
+            # approx -> denorm.append(g * z_low / r_0)
+            denorm.append(np.arcsin(g * z_low / r_0))
+    return np.degrees(np.array(denorm))
 
-w_c = 2 * np.pi * f_c
-beta = w_c * np.sqrt(epsilon) / rf.c
 
-step = 0.01e9
-freq = rf.Frequency(start=step, stop=1.5 * f_c, unit="Hz", npoints=int(f_c / step))
-freq.unit = "GHz"
-med = rf.DefinedGammaZ0(freq, z0=R0, gamma=1j * freq.w * np.sqrt(epsilon) / rf.c)
-elm_list = list()
-for w_cap in w_cap_l:
-    for w_ind in w_ind_l:
-        # short line at the input
-        cir = med.line(10, z0=R0)
-        for i, g in enumerate(norm_coeff):
-            if i == order:
-                print("skipping last g (it's a resistor)")
-                break
-            if i % 2 == 0:
-                C = g / (R0 * w_c)
-                if w_cap:
-                    elm = med.shunt_capacitor(C, name=f"c{i}")
-                    print(f"C{i}={C * 1e15:.1f} fF")
+if __name__ == "__main__":
+    w_c = 2 * np.pi * f_c
+    beta = w_c * np.sqrt(epsilon) / rf.c
+
+    step = 0.01e9
+    freq = rf.Frequency(start=step, stop=1.5 * f_c, unit="Hz", npoints=int(f_c / step))
+    freq.unit = "GHz"
+    med = rf.DefinedGammaZ0(freq, z0=R0, gamma=1j * freq.w * np.sqrt(epsilon) / rf.c)
+    elm_list = list()
+    for w_cap in w_cap_l:
+        for w_ind in w_ind_l:
+            # short line at the input
+            cir = med.line(10, z0=R0)
+            for i, g in enumerate(norm_coeff):
+                if i == order:
+                    print("skipping last g (it's a resistor)")
+                    break
+                if i % 2 == 0:
+                    C = g / (R0 * w_c)
+                    if w_cap:
+                        elm = med.shunt_capacitor(C, name=f"c{i}")
+                        print(f"C{i}={C * 1e15:.1f} fF")
+                    else:
+                        Z = Z_low
+                        l = 0.89 * np.arcsin(g * Z / R0) / beta
+                        elm = med.line(l, name=f"l{i}", unit="m", z0=Z)
+                        print(f"Z{i}={Z}\tl={l * 1e3:.2f} mm\t{beta*l=:.3f}")
                 else:
-                    Z = Z_low
-                    l = 0.89 * np.arcsin(g * Z / R0) / beta
-                    elm = med.line(l, name=f"l{i}", unit="m", z0=Z)
-                    print(f"Z{i}={Z}\tl={l * 1e3:.2f} mm\t{beta*l=:.3f}")
-            else:
-                L = R0 * g / w_c
-                if w_ind:
-                    elm = med.inductor(L, name=f"l{i}")
-                    print(f"L{i}={L * 1e12:.1f} pH")
-                else:
-                    Z = Z_high
-                    l = 2 * np.arctan(g * R0 / (2 * Z)) / beta
-                    elm = med.line(l, name=f"l{i}", unit="m", z0=Z)
-                    print(f"Z{i}={Z}\tl={l * 1e3:.2f} mm\t{beta*l=:.3f}")
-            cir = cir.copy() ** elm
-        cir.name = f"{'cap' if w_cap else 'z_l'} - {'ind' if w_ind else 'z_h'}"
-        cir = cir.copy() ** med.line(10, z0=R0)
+                    L = R0 * g / w_c
+                    if w_ind:
+                        elm = med.inductor(L, name=f"l{i}")
+                        print(f"L{i}={L * 1e12:.1f} pH")
+                    else:
+                        Z = Z_high
+                        l = 2 * np.arctan(g * R0 / (2 * Z)) / beta
+                        elm = med.line(l, name=f"l{i}", unit="m", z0=Z)
+                        print(f"Z{i}={Z}\tl={l * 1e3:.2f} mm\t{beta*l=:.3f}")
+                cir = cir.copy() ** elm
+            cir.name = f"{'cap' if w_cap else 'z_l'} - {'ind' if w_ind else 'z_h'}"
+            cir = cir.copy() ** med.line(10, z0=R0)
--- tests/test_devices/test_app_models/test_filter.py
+++ tests/test_devices/test_app_models/test_filter.py
@@ -3,9 +3,23 @@ from pytest import approx
 from numpy import array
 
 
-def test_filter():
+def test_prototype():
     proto = flr.prototype(5, "flat")
     assert proto == approx(array([0.6180, 1.6180, 2, 1.618, 0.618]), 5)
 
     proto = flr.prototype(3, "ripple", 0.5)
     assert proto == approx(array([1.5963, 1.0967, 1.5963, 1]), 5)
+
+
+def test_scaling():
+    proto1 = array([0.6180, 1.6180, 2, 1.618, 0.618])
+    denorm = flr.scaling(proto1, 2e9, 50)
+    assert denorm == approx(
+        array([0.984e-12, 6.438e-9, 3.183e-12, 6.438e-9, 0.984e-12])
+    )
+
+
+def test_to_stepped_impedance():
+    scaled1 = array([0.517, 1.414, 1.932, 1.932, 1.414, 0.517])
+    beta_l = array([11.93, 32.8, 50.61, 43.85, 34.44, 12.3])
+    assert flr.to_stepped_impedance(scaled1, 120, 20) == approx(beta_l, rel=1e-2)

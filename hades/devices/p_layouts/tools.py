262fa7a683910cb99305b66e5f634a6d6b71c2d0
Patarimi <mpqqch@gmail.com>
2023-12-19 14:04:42 +0100

    implement a diff function to check gds generation

--- hades/devices/p_layouts/tools.py
+++ hades/devices/p_layouts/tools.py
@@ -1,4 +1,6 @@
+import subprocess
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass
@@ -8,3 +10,16 @@ class Layer:
 
     def __str__(self):
         return f"{self.data}/{self.d_type}"
+
+
+def check_diff(gds1: Path, gds2: Path):
+    """
+    Test if the 2 gds files are the same. Raise error if they differe.
+    :param gds1: path of the first gds
+    :param gds2: path of the second gds
+    :return: None
+    """
+    cmd = f"strmxor {gds1} {gds2}"
+    c = subprocess.run(cmd, shell=True, capture_output=True)
+    if c.returncode != 0:
+        raise ValueError(c.stdout)
--- tests/test_devices/test_p_layout/test_inductor.py
+++ tests/test_devices/test_p_layout/test_inductor.py
@@ -1,5 +1,6 @@
 from hades.devices.p_layouts.inductor import octagonal_inductor
-from hades.devices.p_layouts.tools import Layer
+from hades.devices.p_layouts.tools import Layer, check_diff
+import gdstk
 
 
 def test_inductor():
@@ -8,3 +9,7 @@ def test_inductor():
                              5e-6,
                              2e-6,
                              Layer(10, 2))
+    lib = gdstk.Library()
+    lib.add(ind)
+    lib.write_gds("ind.gds")
+    check_diff("ind.gds", "./tests/test_devices/test_p_layout/ind_ref.gds")

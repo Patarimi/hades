7ac848cfc62fcc16431be2ad2ed58371c487428d
Patarimi <mpqqch@gmail.com>
2023-12-19 10:52:34 +0100

    detect pdk not install and skip tests

--- tests/test_generate/test_generate.py
+++ tests/test_generate/test_generate.py
@@ -1,15 +1,24 @@
 import os
 import warnings
 from hades.main import generate_cli
+from hades.techno import get_layer
 from os import chdir
+import yaml
+import pytest
 
 
 def test_generation():
     chdir("./tests/test_generate/")
     if os.name == "nt":
-        warnings.warn(
+        pytest.skip(
             "Windows system not supported for local generation. Skipping Test"
         )
-        return
-    generate_cli(design_yaml="./design ind gf.yml", stop="geometries")
-    generate_cli(design_yaml="./design ms sky.yml", stop="geometries")
+    for design in ("./design ind gf.yml", "./design ms sky.yml"):
+        with open(design) as f:
+            conf = yaml.load(f, Loader=yaml.Loader)
+        pdk = conf["techno"]
+        try:
+            get_layer(pdk, "M6")
+        except FileNotFoundError:
+            pytest.skip(f"The pdk {pdk} is not installed. Skipping")
+        generate_cli(design_yaml=design, stop="geometries")

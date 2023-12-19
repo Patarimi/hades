acca434e8c3de8cef0d7e2bfe9c022fab1207df1
Patarimi <mpqqch@gmail.com>
2023-12-19 16:58:31 +0100

    last try

--- tests/test_devices/test_p_layout/test_tools.py
+++ tests/test_devices/test_p_layout/test_tools.py
@@ -6,4 +6,5 @@ def test_tools():
     assert str(lay) == "100/4"
     lay2 = tools.Layer(141)
     assert str(lay2) == "141/0"
-    tools.check_diff("./tests/test_devices/test_p_layout/ind_ref.gds", "./tests/test_devices/test_p_layout/ind_ref.gds")
+    tools.check_diff("../tests/test_devices/test_p_layout/ind_ref.gds",
+                     "../tests/test_devices/test_p_layout/ind_ref.gds")

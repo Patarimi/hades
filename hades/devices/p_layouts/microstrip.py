1dda46946927505d8ad959c9cad5f5cd920d65fa
Patarimi <mpqqch@gmail.com>
2023-12-20 12:24:15 +0100

    coupled lines p_layout

--- hades/devices/p_layouts/microstrip.py
+++ hades/devices/p_layouts/microstrip.py
@@ -3,14 +3,16 @@ import gdstk
 
 
 def straight_line(
-    width: float,
-    length: float,
-    top_metal: Layer,
-    bot_metal: Layer,
-    ports: [Port, Port] = (Port("S1"), Port("S2")),
+        width: float,
+        length: float,
+        top_metal: Layer,
+        bot_metal: Layer,
+        ports: [Port, Port] = (Port("S1"), Port("S2")),
+        name: str = "ms"
 ) -> gdstk.Cell:
     """
     Generate a micro-strip straight line cell. Can be exported as a gds files.
+    :param name: name of the cell generated (default 'ms')
     :param width: Width of the signal line (Reference line is three time wider).
     :param length: Length of the micro-strip.
     :param top_metal: Layer of the signal line
@@ -20,7 +22,7 @@ def straight_line(
     """
     m_top = top_metal
     m_bott = bot_metal
-    ms = gdstk.Cell("ms")
+    ms = gdstk.Cell(name)
     le = length * 1e6
     w = width * 1e6
     rf = gdstk.RobustPath((0, 0), w, layer=m_top.data, datatype=m_top.d_type)
@@ -37,5 +39,16 @@ def straight_line(
     return ms
 
 
-def coupled_line():
-    ...
+def_port = [Port(name) for name in ("in", "out", "cpl", "iso")]
+
+
+def coupled_line(width1: float, length: float, gap: float, top_metal: Layer, bot_metal: Layer, width2: float = -1,
+                 ports: [Port, ] = def_port, name: str = "cpl"):
+    w2 = width2 if width2 > 0 else width1
+    ms1 = straight_line(width1, length, top_metal, bot_metal, ports[0:2])
+    ms2 = straight_line(w2, length, top_metal, bot_metal, ports[2:])
+    cpl = gdstk.Cell(name)
+    ms_rf = gdstk.Reference(ms1, (0, (width1 * 1e6 + gap * 1e6) / 2))
+    ms_rf2 = gdstk.Reference(ms2, (0, -(w2 * 1e6 + gap * 1e6) / 2))
+    cpl.add(ms_rf, ms_rf2)
+    return cpl.flatten()
--- tests/test_devices/test_p_layout/test_microstrip.py
+++ tests/test_devices/test_p_layout/test_microstrip.py
@@ -2,11 +2,11 @@ from os.path import dirname, join
 
 import gdstk
 
-from hades.devices.p_layouts.microstrip import straight_line
+from hades.devices.p_layouts.microstrip import straight_line, coupled_line
 from hades.devices.p_layouts.tools import Layer, check_diff
 
 
-def test_inductor(tmp_path):
+def test_straight_line(tmp_path):
     ms = straight_line(10e-6,
                        50e-6,
                        Layer(10, 2),
@@ -16,3 +16,17 @@ def test_inductor(tmp_path):
     lib.write_gds(tmp_path / "ms.gds")
     ref_path = dirname(__file__)
     check_diff(tmp_path / "ms.gds", join(ref_path, "ms_ref.gds"))
+
+
+def test_coupler(tmp_path):
+    ref_path = dirname(__file__)
+    ms = coupled_line(10e-6,
+                      50e-6,
+                      20e-6,
+                      Layer(10, 2),
+                      Layer(50)
+                      )
+    lib = gdstk.Library()
+    lib.add(ms)
+    lib.write_gds(join(tmp_path, "cpl.gds"))
+    check_diff(tmp_path / "cpl.gds", join(ref_path, "cpl_ref.gds"))

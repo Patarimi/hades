0a06aae1c01c5c01241e241042ba1a2bcc2eef0b
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-01-08 16:08:01 +0100

    new layer_stack object for technologically-agnostic design.

--- tests/test_devices/test_p_layout/test_tools.py
+++ tests/test_devices/test_p_layout/test_tools.py
@@ -13,3 +13,16 @@ def test_tools():
     ref = join(base_path, "ind_ref.gds")
     print(os.name)
     tools.check_diff(ref, ref)
+
+
+def test_layer_stack():
+    layer_stack = tools.LayerStack("gf180mcu")
+    print(layer_stack)
+    assert layer_stack.stack[0].data == 34
+    assert layer_stack.get_metal_layer(1) == tools.Layer(34, 0, "Metal1")
+    assert layer_stack.get_metal_layer(2) == tools.Layer(36, 0, "Metal2")
+    assert layer_stack.get_metal_layer(-1) == tools.Layer(81, 0, "Metal5")
+
+    assert layer_stack.get_via_layer(1) == tools.Layer(35, 0, "Via1")
+    assert layer_stack.get_via_layer(2) == tools.Layer(38, 0, "Via2")
+    assert layer_stack.get_via_layer(-1) == tools.Layer(41, 0, "Via4")

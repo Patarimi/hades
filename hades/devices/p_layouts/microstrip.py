e7de5f897d9b63afc340cb0316089453ad469118
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-01-08 16:25:23 +0100

    re-write inductor.py and microstrip.py to handle new layerstack file.

--- hades/devices/p_layouts/microstrip.py
+++ hades/devices/p_layouts/microstrip.py
@@ -1,28 +1,27 @@
-from .tools import Layer, Port
+from .tools import LayerStack, Port, Layer
 import gdstk
 import math
 
 
 def straight_line(
-        width: float,
-        length: float,
-        top_metal: Layer,
-        bot_metal: Layer,
-        ports: [Port, Port] = (Port("S1"), Port("S2")),
-        name: str = "ms",
+    width: float,
+    length: float,
+    layerstack: LayerStack,
+    ports: [Port, Port] = (Port("S1"), Port("S2")),
+    name: str = "ms",
 ) -> gdstk.Cell:
     """
     Generate a micro-strip straight line cell. Can be exported as a gds files.
     :param name: name of the cell generated
     :param width: Width of the signal line (Reference line is three time wider).
     :param length: Length of the micro-strip.
-    :param top_metal: Layer of the signal line
-    :param bot_metal: Layer of the reference line
+    :param layerstack: LayerStack object. The highest metal layer will be used for the signal line.
+    The lowest metal layer will be used for the ground plane.
     :param ports: name of the ports
     :return:
     """
-    m_top = top_metal
-    m_bott = bot_metal
+    m_top = layerstack.get_metal_layer(-1)
+    m_bott = layerstack.get_metal_layer(1)
     ms = gdstk.Cell(name)
     le = length * 1e6
     w = width * 1e6
@@ -47,30 +46,29 @@ def_port = tuple(
 
 
 def coupled_lines(
-        width1: float,
-        length: float,
-        gap: float,
-        top_metal: Layer,
-        bot_metal: Layer,
-        width2: float = -1,
-        ports: list[Port] = def_port,
-        name: str = "cpl",
+    width1: float,
+    length: float,
+    gap: float,
+    layerstack: LayerStack,
+    width2: float = -1,
+    ports: list[Port] = def_port,
+    name: str = "cpl",
 ) -> gdstk.Cell:
     """
     Generate a cell with two micro-strip lines coupled by a gap. Can be exported as a gds files.
     :param width1: width of the first line.
     :param length: length of the two lines.
     :param gap: gap between the two lines.
-    :param top_metal: Layer of the signal track.
-    :param bot_metal: Layer of the reference track.
+    :param layerstack: LayerStack object. The highest metal layer will be used for the signal line.
+    The lowest metal layer will be used for the ground plane.
     :param width2: width of the second line.
     :param ports: name of each port.
     :param name: name of the cell.
     :return:
     """
     w2 = width2 if width2 > 0 else width1
-    ms1 = straight_line(width1, length, top_metal, bot_metal, ports[0:2])
-    ms2 = straight_line(w2, length, top_metal, bot_metal, ports[2:])
+    ms1 = straight_line(width1, length, layerstack, ports[0:2])
+    ms2 = straight_line(w2, length, layerstack, ports[2:])
     cpl = gdstk.Cell(name)
     ms_rf = gdstk.Reference(ms1, (0, (width1 * 1e6 + gap * 1e6) / 2))
     ms_rf2 = gdstk.Reference(ms2, (0, -(w2 * 1e6 + gap * 1e6) / 2))
@@ -79,32 +77,35 @@ def coupled_lines(
 
 
 def lange_coupler(
-        width: float,
-        length: float,
-        gap: float,
-        top_metal: tuple[Layer],
-        bot_metal: Layer,
-        ports: list[Port] = def_port,
-        name: str = "lange",
+    width: float,
+    length: float,
+    gap: float,
+    layerstack: LayerStack,
+    ports: list[Port] = def_port,
+    name: str = "lange",
 ) -> gdstk.Cell:
     """
     Generate a flat symmetrical lange coupler.
     :param width: track width (in µm)
     :param length: total length of the lines.
     :param gap: space between each track.
-    :param top_metal: tuple with the main metal, the via and the metal for bridging.
-    :param bot_metal: lower reference metal (ground plane).
+    :param layerstack: LayerStack object. The highest metal layer will be used for the signal line.
+    The lowest metal layer will be used for the ground plane. The second higher metal layer will be used for bridging.
     :param ports: name of each port.
     :param name: name of the returned cell.
     :return:
     """
     ext = 5
     w, l, g = width * 1e6, length * 1e6, gap * 1e6
+    top_metal = layerstack.get_metal_layer(-1)
+    bridge = layerstack.get_metal_layer(-2)
+    top_via = layerstack.get_via_layer(-1)
+    bot_metal = layerstack.get_metal_layer(1)
     first_met = gdstk.FlexPath(
         (0, 0),
         w,
-        layer=top_metal[0].data,
-        datatype=top_metal[0].d_type,
+        layer=top_metal.data,
+        datatype=top_metal.d_type,
         ends="extended",
     )
     first_met.horizontal(l, relative=True)
@@ -112,14 +113,14 @@ def lange_coupler(
     first_met.horizontal(-l, relative=True)
     first_met.vertical(ext, relative=True)
     port = gdstk.FlexPath(
-        (l, 2.5 * w + 2 * g), w, layer=top_metal[0].data, datatype=top_metal[0].d_type
+        (l, 2.5 * w + 2 * g), w, layer=top_metal.data, datatype=top_metal.d_type
     )
     port.vertical(ext, relative=True)
     sec_met = gdstk.FlexPath(
         [(0, 0), (0, 2 * (w + g))],
         w,
-        layer=top_metal[2].data,
-        datatype=top_metal[2].d_type,
+        layer=bridge.data,
+        datatype=bridge.d_type,
         ends="extended",
     )
     via_w = 0.4
@@ -128,7 +129,7 @@ def lange_coupler(
     rep = math.floor((w - 2 * via_s - via_w) / (via_w + via_g)) + 1
     shift = -via_w - (rep - 1) * (via_w + via_g)
     via = gdstk.rectangle(
-        (0, 0), (via_w, via_w), layer=top_metal[1].data, datatype=top_metal[1].d_type
+        (0, 0), (via_w, via_w), layer=top_via.data, datatype=top_via.d_type
     )
     via.repetition = gdstk.Repetition(rep, rep, spacing=(via_g + via_w, via_g + via_w))
     via.translate(shift / 2, shift / 2)
@@ -151,8 +152,8 @@ def lange_coupler(
         lab = gdstk.Label(
             ports[i].name,
             coord[i],
-            layer=top_metal[0].data,
-            texttype=top_metal[0].d_type,
+            layer=top_metal.data,
+            texttype=top_metal.d_type,
         )
         ref = gdstk.Label(
             ports[i].ref, coord[i], layer=bot_metal.data, texttype=bot_metal.d_type
--- tests/test_devices/test_p_layout/test_inductor.py
+++ tests/test_devices/test_p_layout/test_inductor.py
@@ -1,12 +1,13 @@
 from os.path import dirname, join
 
 from hades.devices.p_layouts.inductor import octagonal_inductor
-from hades.devices.p_layouts.tools import Layer, check_diff
+from hades.devices.p_layouts.tools import LayerStack, check_diff
 import gdstk
 
 
 def test_inductor(tmp_path):
-    ind = octagonal_inductor(120e-6, 1, 5e-6, 2e-6, Layer(10, 2))
+    layerstack = LayerStack("gf180mcu")
+    ind = octagonal_inductor(120e-6, 1, 5e-6, 2e-6, layerstack)
     lib = gdstk.Library()
     lib.add(ind)
     lib.write_gds(tmp_path / "ind.gds")

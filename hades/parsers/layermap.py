bba75677a574b1f8bde00414ade583b49664dde1
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-01-24 15:15:24 +0100

    create new layermap parser using lark

---
+++ hades/parsers/layermap.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+from os.path import dirname, isabs, join
+from hades.parsers.tools import parse
+from lark import Transformer
+
+from hades.techno import load_pdk
+
+
+@dataclass
+class Map:
+    types: dict[int, list[str]]
+    layer: int
+
+    def __str__(self):
+        return f"{self.layer} - {self.types}"
+
+    def __getitem__(self, item: str):
+        for key in self.types:
+            if item.lower() in self.types[key]:
+                return self.layer, key
+        raise KeyError(
+            f"Datatype {item} not found." f"Available datatypes are: {self.types}."
+        )
+
+
+class LayerMap(Transformer):
+    INTEGER = int
+    NAME = str
+
+    def SETOFTYPE(self, types):
+        return types.lower().split(",")
+
+    def TYPE(self, types):
+        return types.lower().split(",")
+
+    def layer(self, layer):
+        return layer
+
+    def start(self, start) -> dict[str, Map]:
+        map_d = dict()
+        for layer in start:
+            name = layer[0]
+            if name in map_d.keys():
+                datatype = layer[-1]
+                if datatype in map_d[name].types.keys():
+                    map_d[name].types[datatype] += layer[1]
+                else:
+                    map_d[name].types[datatype] = layer[1]
+            else:
+                map_d[name] = Map({layer[-1]: layer[1]}, layer[-2])
+        return map_d
+
+
+def load_map(techno: str) -> dict[str, Map]:
+    pdk = load_pdk(techno)
+    if isabs(pdk["base_dir"]):
+        map_path = join(pdk["base_dir"], pdk["layermap"])
+    else:
+        map_path = join(dirname(dirname(__file__)), pdk["base_dir"], pdk["layermap"])
+    t = parse(map_path, "layermap")
+    map_list = LayerMap().transform(t)
+    return map_list
+
+
+def get_number(techno: str, name: str, datatype: str = "drawing") -> tuple[int, int]:
+    """
+    Read layer information (layer number and datatype) from layermap file.
+    :param techno: name of the selected technology.
+    :param name: name of the layer
+    :param datatype: type of the data (drawing, pin, etc.)
+    :return: layer number and datatype
+    """
+    layer_info = load_map(techno)
+    if name not in layer_info:
+        raise KeyError(f"Layer {name} not found in {techno}")
+    return layer_info[name][datatype]

78328313f184a1168a2aab7220434b1c43ddb2a9
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-01-24 15:14:43 +0100

    move parse function out of spice.py

--- hades/parsers/spice.py
+++ hades/parsers/spice.py
@@ -1,16 +1,4 @@
-from pathlib import Path
-
-from lark import Lark, Transformer, Tree
-from os.path import join, dirname
-
-
-def parse(file: str | Path, template: str = "spice") -> Tree:
-    tpt_file = join(dirname(__file__), template + ".lark")
-    with open(tpt_file, "r") as f:
-        spice_parser = Lark(f)
-    with open(file) as f:
-        t = spice_parser.parse(f.read())
-    return t
+from lark import Transformer
 
 
 class SpiceTransformer(Transformer):
---
+++ hades/parsers/tools.py
@@ -0,0 +1,16 @@
+"""
+This package give general tools to help parse file with lark.
+"""
+from pathlib import Path
+
+from lark import Lark, Tree
+from os.path import join, dirname
+
+
+def parse(file: str | Path, template: str = "spice") -> Tree:
+    tpt_file = join(dirname(__file__), template + ".lark")
+    with open(tpt_file, "r") as f:
+        spice_parser = Lark(f)
+    with open(file) as f:
+        t = spice_parser.parse(f.read())
+    return t

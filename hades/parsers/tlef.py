2d98d174fc54316bac1f9857753d619ddecd945d
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-02-13 15:02:56 +0100

    rewrite parser

--- hades/parsers/tlef.py
+++ hades/parsers/tlef.py
@@ -2,12 +2,22 @@
 List of function to parse technological lef (TLEF) files.
 TLEF files give the information on the back-end composition and associated design rules.
 """
+
+import dataclasses
+import logging
+from enum import Enum
 from pathlib import Path
-
-import lark
-
 from .tools import parse
-from lark import Transformer
+from lark import Discard, Transformer
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dataclasses.dataclass
+class Layer:
+    name: str
+    ltype: Enum("ROUTING", "CUT")
+    width: float
 
 
 class TechLef(Transformer):
@@ -15,39 +25,51 @@ class TechLef(Transformer):
     FLOAT = float
     WORD = str
     BLOCKNAME = str
+    KEYWORD = str
+    item = list
+
+    def table(self, _):
+        return Discard
+
+    def list(self, _):
+        return Discard
 
     def setting(self, setting):
-        if len(setting) > 2:
-            if setting[1] == "RANGE":
-                return {setting[0]: setting[2]}
-            if setting[0] in ("BELOW", "ABOVE"):
-                return {setting[0]: setting[1:]}
-            else:
-                return setting
-        return setting[0] if len(setting) == 1 else {setting[0]: setting[1]}
+        return setting[0] if len(setting) == 1 else setting
 
-    def item(self, item):
-        return {item[0]: item[1]}
-
-    def lef58_property(self, lef58_property):
-        return {lef58_property[2]: lef58_property[3]}
+    def lef58_property(self, _):
+        return Discard
 
     def block(self, block):
-        if block[0] == "LAYER":
-            item_dict = dict()
-            for item in block[2:-1]:
-                key = list(item.keys())[0]
-                if key in item_dict:
-                    if isinstance(item_dict[key], list):
-                        item_dict[key].append(item[key])
-                    elif isinstance(item_dict[key], dict):
-                        item_dict[key].update(item[key])
-                    else:
-                        item_dict.update({key: [item_dict[key], item[key]]})
+        if block[0] != "LAYER":
+            # print(f"Discarding block: {block}")
+            return Discard
+        if block[1] != block[-1]:
+            raise ValueError(f"Block name does not match ({block[1]} and {block[-1]}")
+        item_dict = dict()
+        # print(f"In block: {block=}")
+        for item in block[2:-1]:
+            if not item:
+                print("list is empty, skipping")
+                continue
+            # print(f"In block: {item=}")
+            key = item[0]
+            if key in item_dict:
+                print(f"In block: {item_dict[key]=}\t{item[1:]=}")
+                if item_dict[key] == item[1:] or item_dict[key] == item[1]:
+                    # redundant item, skip
+                    continue
+                print(type(item_dict[key]))
+                if isinstance(item_dict[key], float):
+                    # previous item was a float, convert to list
+                    item_dict[key] = [item_dict[key], "UP_TO", item[3]] + item[1:]
                 else:
-                    item_dict.update(item)
-            return {block[1]: item_dict}
-        return block
+                    item_dict[key] += item[1:]
+
+            else:
+                item_dict.update({item[0]: item[1:] if len(item) > 2 else item[1]})
+        # print(f"In block: {item_dict=}")
+        return {block[1]: item_dict}
 
     def start(self, start):
         ss = dict()
@@ -81,6 +103,8 @@ def get_all_by_type(l_type: str, tlef_path: Path) -> list[str]:
     for layer in full_stack:
         if full_stack[layer]["TYPE"] == l_type:
             layers.append(layer)
+    if not layers:
+        raise ValueError(f"No layer of type {l_type} found in {full_stack}")
     return layers
 
 
@@ -95,7 +119,11 @@ def get_by_type(l_type: str, tlef_path: Path, nbr: int) -> str:
     """
     if nbr == 0:
         raise ValueError("nbr cannot be 0")
-    return get_all_by_type(l_type, tlef_path)[nbr - 1 if nbr > 0 else nbr]
+    layers = get_all_by_type(l_type, tlef_path)
+    try:
+        return layers[nbr - 1 if nbr > 0 else nbr]
+    except IndexError:
+        raise IndexError(f"List out of range with {nbr} in {layers}")
 
 
 def get_metal(nbr: int, tlef_path: Path) -> str:
--- tests/test_parser/test_tlef.py
+++ tests/test_parser/test_tlef.py
@@ -2,15 +2,16 @@ import pytest
 from hades.parsers.tlef import load_tlef, get_metal, get_via
 from hades.techno import load_pdk
 from os.path import join, dirname, isdir
+import logging
 
 pytestmark = pytest.mark.skipif(not (isdir("./pdk/mock")), reason="PDK not installed.")
 
 
-def test_load_tlef():
+def test_load_tlef(tmp_path):
     pdk = load_pdk("mock")
     path = join(dirname(dirname(__file__)), pdk["base_dir"], pdk["techlef"])
     layers = load_tlef(path)
-    print(layers)
+    logging.debug(layers)
 
     assert get_metal(1, path) == "Metal1"
     assert get_metal(-1, path) == "Metal5"
@@ -19,3 +20,7 @@ def test_load_tlef():
     assert get_via(1, path) == "CON"
 
     assert layers["Via2"]["WIDTH"] == 0.4
+
+
+if __name__ == "__main__":
+    test_load_tlef("./tmp")

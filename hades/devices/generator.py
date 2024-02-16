a44066d900333f86b4b513d8f25e1c54f051faa4
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-02-16 10:08:05 +0100

    first implementation of a generator will remplace device class

---
+++ hades/devices/generator.py
@@ -0,0 +1,52 @@
+import os
+from enum import Enum
+from typing import Callable
+
+import gdstk
+from pydantic import BaseModel
+
+
+class Technology(str, Enum):
+    sky130 = "sky130"
+    gf180mcu = "gf180mcu"
+
+
+class Parameters(BaseModel):
+    """
+    List of parameters for a device.
+    """
+
+    name: str
+
+
+def load(path: str) -> Parameters:
+    """Load a parameter file.
+
+    :param path: Path to the parameter file.
+    :return: A dictionary with the parameters.
+    """
+
+    with open(path) as f:
+        return Parameters.model_validate_json(f.read())
+
+
+class Generator(BaseModel):
+    name: str
+    techno: Technology
+    targets: Parameters
+    parameters_model: Parameters
+    approx_model: Callable[[Parameters, Parameters], Parameters]
+    layout: Callable[[Parameters], gdstk.Cell]
+    accu_model: Callable[[gdstk.Cell], Parameters]
+    calibrator: Callable[[Parameters, Parameters], Parameters]
+
+    def generate(self) -> gdstk.Cell:
+        """
+        Generate the device.
+        :return: a gds cell with the device generated.
+        """
+
+
+if __name__ == "__main__":
+    root = os.path.dirname(os.path.abspath(__file__))
+    print(load(root + "/test.json"))

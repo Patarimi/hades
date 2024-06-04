b2bd9ef88ca3c8fe372eedacf0ef66271ffcee7c
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-06-04 16:07:44 +0200

    fix path

--- tests/test_wrappers/test_npspice.py
+++ tests/test_wrappers/test_npspice.py
@@ -1,7 +1,8 @@
+import pathlib
 from hades.wrappers.ngspice import NGSpice
 
 
 def test_ngspice(tmp_path):
     spice = NGSpice()
-    spice.compute("./tests/test_simulator/schem_test.net", tmp_path / "out.txt")
+    spice.compute("./tests/test_wrappers/schem_test.net", tmp_path / "out.txt")
     assert pathlib.Path(tmp_path / "out.txt").exists()

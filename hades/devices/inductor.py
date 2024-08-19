373e9b7a8bacd23806447603798fcde304902def
Patarimi <mpqqch@gmail.com>
2024-08-19 16:00:49 +0200

    update inductor.py using pydantic

--- hades/devices/inductor.py
+++ hades/devices/inductor.py
@@ -1,4 +1,6 @@
-from .device import Parameters
+from pydantic import BaseModel
+
+from .device import ParamSet
 import gdstk
 from math import pi
 from pathlib import Path
@@ -9,70 +11,89 @@ from ..layouts.tools import LayerStack
 from typing import Optional
 
 
+class Dimensions(BaseModel):
+    n: int
+    W: float
+    G: float
+    d_i: float
+
+
+class Parameters(BaseModel):
+    K1: float = 2.3
+    K2: float = 3.83
+
+class Specifications(BaseModel):
+    L: float = 1e-9
+    f_0: float = 1e9
+
+
 class Inductor:
     name: str
-    specifications: Parameters
-    dimensions: Parameters  # n, W, G, d_i
+    specifications: Specifications
+    dimensions: Dimensions
     parameters: Parameters
     techno: str
 
-    def __init__(self, name: str, techno: str, L: float = 1e-9, f_0: float = 1e9):
+    def __init__(self, name: str, techno: str, specifications: Specifications = Specifications()):
         self.name = name
-        self.specifications = {"L": float(L), "f_0": float(f_0)}
-        self.parameters = {"K1": 2.3, "K2": 3.83}
+        self.specifications = specifications
+        self.parameters = Parameters()
         self.techno = techno
         self.em = Emx()
         self.em.prepare(techno)
 
-    def update_model(self, specifications: Optional[Parameters] = None) -> Parameters:
+    def update_model(self, specifications: Specifications = None) -> Dimensions:
         if specifications is not None:
-            self.specifications = specifications
-        self.dimensions = {"n": 1, "W": 10e-6}
+            if type(specifications) is dict:
+                self.specifications = Specifications(**specifications)
+            else:
+                self.specifications = specifications
+        self.dimensions = Dimensions(n=1, W=10e-6, G=10e-6, d_i=100e-6)
 
         def ind_di(x):
             return abs(
-                self.ind_value(x, self.parameters["K1"])
-                - float(self.specifications["L"])
+                self.ind_value(x, self.parameters.K1)
+                - float(self.specifications.L)
             )
 
         res = minimize_scalar(ind_di, bounds=(0, 1e-3))
-        self.dimensions["d_i"] = res.x
+        self.dimensions.d_i = res.x
         return self.dimensions
 
     def ind_value(self, d_i: float, k1: float) -> float:
-        k2 = float(self.parameters["K2"])
+        k2 = float(self.parameters.K2)
         u_0 = 4 * pi * 1e-7
         if "W" in self.dimensions:
-            d_o = float(self.dimensions["W"]) + d_i
+            d_o = float(self.dimensions.W) + d_i
         else:
-            self.dimensions["W"] = 10e-6
+            self.dimensions.W = 10e-6
             d_o = d_i + 10
         rho = (d_o - d_i) / (d_o + d_i)
         d_avg = (d_o + d_i) / 2
         return k1 * u_0 * d_avg / (1 + k2 * rho)
 
-    def update_cell(self, dimensions: Parameters) -> gdstk.Cell:
-        self.dimensions = dimensions
+    def update_cell(self, dimensions: Dimensions) -> gdstk.Cell:
+        self.dimensions = dimensions if type(dimensions) is Dimensions else Dimensions(**dimensions)
         layer_stack = LayerStack(self.techno)
         ind = octagonal_inductor(
-            float(dimensions["d_i"]),
-            int(dimensions["n"]),
-            float(dimensions["W"]),
+            self.dimensions.d_i,
+            self.dimensions.n,
+            self.dimensions.W,
             1e-6,
             layer_stack,
         )
 
         return ind
 
-    def update_accurate(self, sim_file: Path) -> Parameters:
-        f_0 = float(self.specifications["f_0"])
+    def update_accurate(self, sim_file: Path) -> Specifications:
+        f_0 = float(self.specifications.f_0)
         Y = self.em.compute(sim_file, self.name, f_0)
-        return {"L": -(1 / Y.y[0, 0, 1]).imag / (2 * pi * float(f_0)), "f_0": f_0}
+        return Specifications(L =-(1 / Y.y[0, 0, 1]).imag / (2 * pi * float(f_0)), f_0 = f_0)
 
-    def recalibrate_model(self, performances: Parameters) -> Parameters:
+    def recalibrate_model(self, performances: Specifications) -> Parameters:
         def ind_k1(x):
-            return abs(self.ind_value(self.dimensions["d_i"], x) - performances["L"])
+            return abs(self.ind_value(self.dimensions.d_i, x) - performances.L)
 
         res = minimize_scalar(ind_k1, bounds=(0, 20))
-        self.parameters["K1"] = res.x
+        self.parameters.K1 = res.x
         return self.parameters

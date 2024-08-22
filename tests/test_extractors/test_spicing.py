9c14affda5e85d205cc1c20a7eba76f4b7485ad4
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-08-22 16:34:00 +0200

    add extraction testing

--- tests/test_extractors/test_spicing.py
+++ tests/test_extractors/test_spicing.py
@@ -1,16 +1,30 @@
 import filecmp
 from os.path import join, dirname
 
-from hades.extractors.spicing import extract_spice
+from hades.extractors.spicing import extract_spice, extract_spice_magic
+from hades.extractors.tools import check_diff
 from pathlib import Path
 
 
 def test_spice_extractor(tmp_path):
-    output_path = tmp_path / "spice.spice"
+    output_path = tmp_path / "spice.cir"
     extract_spice(
         Path("tests/test_layouts/ref_ind.gds"), techno="sky130", output_path=output_path
     )
     assert output_path.exists()
     filecmp.cmp(
-        output_path, join(dirname(__file__), "../test_extractors/ref_ind.spice")
+        output_path, join(dirname(__file__), "../test_extractors/ref_ind.cir")
+    )
+
+
+def test_spice_extractor_magic(tmp_path):
+    output_path = tmp_path / "spice.cir"
+    extract_spice_magic(
+        Path("tests/test_extractors/sky130_fd_sc_hd.gds"),
+        Path("pdk/sky130A/libs.tech/magic/sky130A.magicrc"),
+        output_path=output_path,
+    )
+    assert output_path.exists()
+    assert check_diff(
+        output_path, join(dirname(__file__), "ref_sky130_fd.cir")
     )

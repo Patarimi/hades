92efd0f156c5a29bba81a7d1378ab91c31d081b5
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-10-02 15:46:05 +0200

    work toward a working example

--- workdir/generate.py
+++ workdir/generate.py
@@ -1,6 +1,49 @@
-from hades.devices.device import generate, Step
-from hades.devices.inductor import Specifications, Inductor
+from pathlib import Path
 
-spec = Specifications(L=1e-9, f_0 = 1e9)
+import numpy as np
+from matplotlib import pyplot as plt
+from skrf import Network
 
-generate(Inductor("test", "sky130"), spec, stop=Step.dimensions)
+from hades.wrappers.oems import compute
+from hades.layouts.tools import Port
+
+
+if __name__ == "__main__":
+    post_proc_only = False
+    if not post_proc_only:
+        s_res = compute(
+            Path("../tests/test_layouts/ref_ind.gds"),
+            "inductor",
+            (0, 5e9),
+            ports=[Port("in")],
+            sim_path="./inductor",
+            refresh_mesh=False,
+            show_model=True,
+            skip_run=True,
+        )
+        s_res.write_touchstone("inductor.s2p")
+    else:
+        s_res = Network("inductor.s2p")
+
+    Zin = s_res.z
+    f = s_res.frequency
+
+    # plot feed point impedance
+    fig, ax = plt.subplots(2, 1)
+    plt.title("feed point impedance")
+    ax[0].plot(f / 1e6, np.real(Zin), "k-", linewidth=2, label=r"$\Re(Z_{in})$")
+    plt.grid()
+    ax[0].legend()
+    ax[0].grid()
+    ax[1].plot(
+        f / 1e6,
+        np.imag(Zin) / (2 * np.pi * f),
+        "r--",
+        linewidth=2,
+        label=r"$\Im(Z_{in})$",
+    )
+    plt.xlabel("frequency (MHz)")
+    plt.ylabel("Inductance ($H$)")
+    plt.legend()
+    plt.tight_layout()
+    plt.show()

add3310d659804a59fd226392d73bca4ef6c2878
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-10-02 12:08:42 +0200

    stl file is not consistent between windows and linux

--- tests/test_wrappers/test_ngsolve.py
+++ tests/test_wrappers/test_ngsolve.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 from hades.wrappers.ngsolve_w import write_stl
 from hades.layouts import tools
-from filecmp import cmp
 
 
 def test_ngsolve(tmp_path):
     layer_stack = tools.LayerStack("mock")
     write_stl(Path("tests/test_layouts/ref_ind.gds"), layer_stack, Path(tmp_path / "test_mesh.stl"))
     assert (tmp_path / "test_mesh.stl").exists()
-    assert cmp(tmp_path / "test_mesh.stl", "tests/test_wrappers/ref_mesh.stl")

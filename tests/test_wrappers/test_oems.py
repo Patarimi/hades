583551970a7d9995eff3bdaa8c8cda909912cea1
Patarimi <38954040+Patarimi@users.noreply.github.com>
2024-11-12 09:57:01 +0100

    fix check for new interface

--- tests/test_wrappers/test_oems.py
+++ tests/test_wrappers/test_oems.py
@@ -10,8 +10,9 @@ from hades.wrappers.oems import compute, Frequency
 def test_compute(tmp_path):
     compute(
         Path(dirname(__file__)) / "../test_layouts/ref_ind2.gds",
+        "mock",
         "ind",
-        Frequency(start=0, stop=1e9),
+        (0, 1e9),
         sim_path=tmp_path,
         skip_run=True,
     )

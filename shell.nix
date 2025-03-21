5c9596c94098ede413d0b585da7f63c1bbfdc4c4
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-03-21 14:31:19 +0100

    remove openEMS (unused) bump version from 24.05 -> 24.11

--- shell.nix
+++ shell.nix
@@ -1,13 +1,12 @@
 #!/usr/bin/env nix-shell
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     magic-vlsi
-    openems
   ];
 
   shellHook =

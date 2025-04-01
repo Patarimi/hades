f472805c0643bd429be431822a637c527dd06e5a
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-04-01 17:35:01 +0200

    rewrite inductor.py to use klayout instead of gdstk

--- tests/test_layouts/test_inductor.py
+++ tests/test_layouts/test_inductor.py
@@ -2,7 +2,7 @@ from os.path import dirname, join, isdir
 import pytest
 from hades.layouts.inductor import octagonal_inductor
 from hades.layouts.tools import LayerStack, check_diff
-import gdstk
+import klayout.db as db
 
 REF_PATH = dirname(__file__)
 pytestmark = pytest.mark.skipif(not isdir("./pdk/mock"), reason="PDK not installed.")
@@ -10,18 +10,16 @@ pytestmark = pytest.mark.skipif(not isdir("./pdk/mock"), reason="PDK not install
 
 def test_inductor(tmp_path):
     layerstack = LayerStack("mock")
-    ind = octagonal_inductor(
-        120e-6, 1, 5e-6, 2e-6, layerstack, port_gap=15e-6, port_ext=20e-6
+    lib = db.Layout()
+    octagonal_inductor(
+        lib, 120e-6, 1, 5e-6, 2e-6, layerstack, port_gap=15e-6, port_ext=20e-6
     )
-    lib = gdstk.Library(precision=layerstack.grid)
-    lib.add(ind)
-    lib.write_gds(tmp_path / "ind.gds")
-    check_diff(tmp_path / "ind.gds", join(REF_PATH, "ref_ind.gds"))
+    lib.write(tmp_path / "ind.gds")
+    assert check_diff(tmp_path / "ind.gds", join(REF_PATH, "ref_ind.gds"))
 
-    ind2 = octagonal_inductor(
-        80e-6, 2, 5e-6, 2e-6, layerstack, port_gap=10e-6, port_ext=15e-6
+    lib  = db.Layout()
+    octagonal_inductor(
+        lib, 80e-6, 2, 5e-6, 2e-6, layerstack, port_gap=10e-6, port_ext=15e-6
     )
-    lib = gdstk.Library(precision=layerstack.grid)
-    lib.add(ind2)
-    lib.write_gds(tmp_path / "ind2.gds")
-    check_diff(tmp_path / "ind2.gds", join(REF_PATH, "ref_ind2.gds"))
+    lib.write(tmp_path / "ind2.gds")
+    assert check_diff(tmp_path / "ind2.gds", join(REF_PATH, "ref_ind2.gds"))

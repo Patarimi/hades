5476f7dbaa9bf3977c4db4d00085f446bdb5f5e5
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-04-01 16:27:05 +0200

    rewrite general.py to use klayout instead of gdstk

--- tests/test_layouts/test_general.py
+++ tests/test_layouts/test_general.py
@@ -1,4 +1,4 @@
-import gdstk
+import klayout.db as kl
 
 from hades.layouts.general import via, via_stack, ground_plane
 from hades.layouts.tools import LayerStack, check_diff
@@ -11,30 +11,26 @@ REF_PATH = dirname(__file__)
 def test_via(tmp_path):
     opnng = stack.get_via_layer(-1)
     assert opnng.name == "Opening"
-    v = via(stack.get_via_layer(2), (3, 4))
-    lib = gdstk.Library("toto")
-    lib.add(v)
-    lib.write_gds(tmp_path / "via.gds")
+    lib = kl.Layout()
+    via(lib, stack.get_via_layer(2), (3, 4))
+    lib.write(tmp_path / "via.gds")
     check_diff(tmp_path / "via.gds", join(REF_PATH, "ref_via.gds"))
 
 
 def test_via_stack(tmp_path):
-    v = via_stack(stack, 2, 1, (3, 4))
-    lib = gdstk.Library("toto", precision=stack.grid)
-    lib.add(v)
-    lib.write_gds(tmp_path / "via_stack.gds")
+    lib = kl.Layout()
+    via_stack(lib, stack, 2, 1, (3, 4))
+    lib.write(tmp_path / "via_stack.gds")
     check_diff(tmp_path / "via_stack.gds", join(REF_PATH, "ref_via_stack.gds"))
 
-    v = via_stack(stack, -3, -4, (3, 4))
-    lib = gdstk.Library("toto")
-    lib.add(v)
-    lib.write_gds(tmp_path / "via_stack_neg.gds")
+    lib = kl.Layout()
+    via_stack(lib, stack, -3, -4, (3, 4))
+    lib.write(tmp_path / "via_stack_neg.gds")
     check_diff(tmp_path / "via_stack_neg.gds", join(REF_PATH, "ref_via_stack.gds"))
 
 
 def test_ground_plane(tmp_path):
-    v = ground_plane(stack, (3, 4), 1)
-    lib = gdstk.Library("toto", precision=stack.grid)
-    lib.add(v)
-    lib.write_gds(tmp_path / "ground_plane.gds")
+    lib = kl.Layout()
+    ground_plane(lib, stack, (3, 4), 1)
+    lib.write(tmp_path / "ground_plane.gds")
     check_diff(tmp_path / "ground_plane.gds", join(REF_PATH, "ref_ground_plane.gds"))

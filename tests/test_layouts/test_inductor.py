c2074d2f898a072c5dc0906c59204004923e431c
Patarimi <38954040+Patarimi@users.noreply.github.com>
2025-04-01 15:19:54 +0200

    fix diff check in test_general.py and test_inductor.py

--- tests/test_layouts/test_inductor.py
+++ tests/test_layouts/test_inductor.py
@@ -13,15 +13,11 @@ def test_inductor(tmp_path):
     ind = octagonal_inductor(
         120e-6, 1, 5e-6, 2e-6, layerstack, port_gap=15e-6, port_ext=20e-6
     )
-    lib = gdstk.Library(precision=layerstack.grid)
-    lib.add(ind)
-    lib.write_gds(tmp_path / "ind.gds")
-    check_diff(tmp_path / "ind.gds", join(REF_PATH, "ref_ind.gds"))
+    lib.write(tmp_path / "ind.gds")
+    assert check_diff(tmp_path / "ind.gds", join(REF_PATH, "ref_ind.gds"))
 
     ind2 = octagonal_inductor(
         80e-6, 2, 5e-6, 2e-6, layerstack, port_gap=10e-6, port_ext=15e-6
     )
-    lib = gdstk.Library(precision=layerstack.grid)
-    lib.add(ind2)
-    lib.write_gds(tmp_path / "ind2.gds")
-    check_diff(tmp_path / "ind2.gds", join(REF_PATH, "ref_ind2.gds"))
+    lib.write(tmp_path / "ind2.gds")
+    assert check_diff(tmp_path / "ind2.gds", join(REF_PATH, "ref_ind2.gds"))

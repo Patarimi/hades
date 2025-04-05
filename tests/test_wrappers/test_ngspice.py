e72f692969ff600c1931fc9246df0ba5511b4737
Patarimi <mpqqch@gmail.com>
2025-04-05 12:21:22 +0200

    remove Command and Date line before comparison.

--- tests/test_wrappers/test_ngspice.py
+++ tests/test_wrappers/test_ngspice.py
@@ -11,8 +11,8 @@ def test_ngspice(tmp_path):
     spice.compute("./tests/test_wrappers/schem_test.net", data_file)
     assert pathlib.Path(data_file).exists()
     assert pathlib.Path(tmp_path / "out.log").exists()
-    # remove the line with the date before comparaison
-    for i, line in enumerate(fileinput.input(data_file, inplace=True)):
-        if i != 1:
+    # remove the line with the date before comparison
+    for line in fileinput.input(data_file, inplace=True):
+        if not line.startswith("Date:") and not line.startswith("Command:"):
             print(line, end="")
     assert cmp("./tests/test_parser/test_data/inv.raw", tmp_path / "out.raw")

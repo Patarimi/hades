71043b66da00c6532e0641486403880b1e6fbec6
Patarimi <mpqqch@gmail.com>
2025-04-06 16:17:55 +0200

    remove gdstk from test_mos.py

--- tests/test_layouts/test_mos.py
+++ tests/test_layouts/test_mos.py
@@ -1,8 +1,8 @@
 from os.path import dirname, join
 import pytest
+from klayout import db
 
 from hades.devices.mos import Mos, Dimensions
-import gdstk
 from hades.layouts.tools import check_diff
 
 REF_PATH = dirname(__file__)
@@ -11,9 +11,10 @@ REF_PATH = dirname(__file__)
 @pytest.mark.skip(reason="To be rewrite with active branch")
 def test_mos(tmp_path):
     mos = Mos()
+    lib = db.Layout()
     cell = mos.update_cell(
         Dimensions(n=1, w=130e-9, le=2e-6), {"pplus": (1, 0), "poly": (2, 0)}
     )
-    lib = gdstk.Library().add(cell)
-    lib.write_gds(tmp_path / "mos.gds")
+    lib = lib.add_cell(cell)
+    lib.write(tmp_path / "mos.gds")
     assert check_diff(tmp_path / "mos.gds", join(REF_PATH, "ref_mos.gds"))

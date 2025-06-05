b61d296bb59c80c8a79a1eb64c2f122f3d830131
Patarimi <mpqqch@gmail.com>
2025-06-05 14:11:07 +0200

    add parse_out to support both ngspice output formats

--- hades/parsers/raw.py
+++ hades/parsers/raw.py
@@ -1,8 +1,15 @@
+import logging
+
 import pandas as pd
 from pathlib import Path
 
 
 def parse_raw(results: Path) -> pd.DataFrame:
+    """
+    Read ngspice output (in single line output format) and load it in a dataframe.
+    :param results: file to be loaded.
+    :return: dataframe with loaded data
+    """
     data = dict()
     with open(results, "r") as f:
         for line in f.readlines():
@@ -19,4 +26,32 @@ def parse_raw(results: Path) -> pd.DataFrame:
                         else:
                             data[head].append(float(val))
     df = pd.DataFrame(data=data, dtype=float)
+    return df
+
+def parse_out(results: Path) -> pd.DataFrame:
+    """
+    Read ngspice output (in multiline output format) and load it in a dataframe.
+    :param results: file to be loaded.
+    :return: dataframe with loaded data
+    """
+    data = dict()
+    keys = list()
+    current_bloc = "Header"
+    with open(results, "r") as f:
+        for num, line in enumerate(f.readlines()):
+            words = line.split()
+            if len(words) == 1 and words[0].endswith(":"):
+                current_bloc = words[0].rstrip(":")
+                continue
+            if current_bloc == "Variables":
+                data[words[1]] = list()
+                keys.append(words[1])
+            if current_bloc == "Values" and len(words) == 2:
+                index = 0
+                data[keys[index]].append(words[1])
+            if current_bloc == "Values" and len(words) == 1:
+                index += 1
+                data[keys[index]].append(words[0])
+    df = pd.DataFrame(data=data, dtype=float)
+    logging.debug(df.info)
     return df
--- tests/test_parser/test_raw.py
+++ tests/test_parser/test_raw.py
@@ -1,6 +1,10 @@
-from hades.parsers.raw import parse_raw
+from hades.parsers.raw import parse_raw, parse_out
 
 
 def test_parse_raw():
     df = parse_raw("./tests/test_parser/test_data/schem_test.out")
     assert len(df["time"]) == 508
+
+def test_parse_out():
+    df = parse_out("./tests/test_parser/test_data/inv.raw")
+    assert len(df["v(out)"]) == 508
